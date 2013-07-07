f8f11949be7b866d4fd8515bfeda30ed8346740e
marcin <marcin.gornik@gmail.com>
2013-07-07 22:16:22 +0200

    Removed obsolete tests.

--- test/metatron_game_tests.erl
+++
@@ -1,8 +0,0 @@
--ifdef(TEST).
--include_lib("eunit/include/eunit.hrl").
--module(metatron_game_tests).
-
-create_game_creates_game_with_no_clients_test() ->
-    Game = metatron_game:create(),
-    ?assertEqual({game, []}, Game).
--endif.
