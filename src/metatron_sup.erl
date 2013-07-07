52f628d0b16676b2081a063e98827a56009f1719
marcin <marcin.gornik@gmail.com>
2013-07-07 21:29:01 +0200

    Add edoc documentation to metatron_sup, use OTP template for supervisor.

--- src/metatron_sup.erl
+++ src/metatron_sup.erl
@@ -1,20 +1,42 @@
+%%%-------------------------------------------------------------------
+%%% @doc Main Metatron application supervisor.
+%%%
+%%% The main application supervisor, responsible for supervising the
+%%% game server.
+%%% @see metatron_game_server.
+%%% @end
+%%%-------------------------------------------------------------------
 
 -module(metatron_sup).
 
 -behaviour(supervisor).
 
+%% external API
 -export([start_link/0]).
+
+%% supervisor callbacks
 -export([init/1]).
 
-%% Helper macro for declaring children of supervisor
--define(CHILD(I, Type, Params), {I, {I, start_link, Params}, permanent, 5000, Type, [I]}).
 
+%% ===================================================================
+%% Public API
+%% ===================================================================
+
+%% @doc Start the supervisor.
 start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
+
+%% ===================================================================
+%% supervisor callbacks
+%% ===================================================================
+
+%% @private
+%% @doc Supervisor init function.
 init([]) ->
-    {ok, { {one_for_one, 5, 10}, 
-        [
-            ?CHILD(metatron_game_server, worker, [self()])
-        ]}}.
+    ChildProcess = {metatron_game_server, {metatron_game_server, start_link, []},
+        permanent, 5000, worker, [metatron_game_server]},
+    Children = [ChildProcess],
+    RestartStrategy = {one_for_one, 5, 10},
+    {ok, {RestartStrategy, Children}}.
 
